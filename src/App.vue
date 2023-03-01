16d2af94cff62db8210d1aecb3a7448b16241312
stefanodisanza <disanzastefano@gmail.com>
2023-03-01 16:11:29 +0100

    START

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+ <body>
+  <div class="header">
+    HEADER
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div class="content">
+    Content goes here
+  </div>
+  <div class="big-icons">
+    contiene icone grandi
+  </div>
+  <div class="main">
+    spazio grande con img di sfondo
+  </div>
+  <footer>
+    contiene il footer
+  </footer>
+  
+ </body>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
+
 </style>
